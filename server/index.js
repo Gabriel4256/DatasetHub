4b41f08e610c6f3eaf7fe297c7482638b268382c
Gabriel4256 <tla4256@gmail.com>
2019-11-29 02:41:46 +0900

    conflict solved

--- server/index.js
+++ server/index.js
@@ -3,19 +3,38 @@ var path = require('path');
 var api = require('./routes');
 
 var app = express();
-const portnum = 80
+const prodPort = 80
+const devPort = 3000
 
 // router for home page
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use('/api', api);
-app.use(express.static(path.join(__dirname, '/dist')))
-app.get('/', (req, res) => {
+
+var resourceDirectory=""
+if(process.env.NODE_ENV === 'development'){
+    resourceDirectory = path.resolve(__dirname, './../frontend/public');
+}
+else{
+    resourceDirectory = path.resolve(__dirname, './../frontend/dist');
+}
+
+app.use('/', express.static(resourceDirectory))
+
+app.get('*', (req, res) => {
     
-    res.sendFile(path.join(__dirname,'/dist/index.html'));
+    res.sendFile(resourceDirectory + '/index.html');
 });
 
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
+var portnum = prodPort
+if(process.env.NODE_ENV === 'development'){
+    portnum = devPort
+}
 app.listen(portnum, function() {
     console.log(`Listening on ${portnum}`);
 });
