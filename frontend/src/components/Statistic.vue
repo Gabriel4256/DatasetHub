941b0e0759f53106ba33bcd16dd3ca429dc77aa6
Gabriel4256 <tla4256@gmail.com>
2019-12-22 06:26:03 +0900

    reftr: removed unnecessary dependencies & modulization

---
+++ frontend/src/components/Statistic.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-md-3 col-sm-6 border-right py-5 border-bottom">
+        <div class="animated-digits mx-auto text-center"  v-bind:data-number="dataNumber">
+        <h5 class="animated-digits-digit"><span>0</span>{{dataUnit}}</h5>
+        <p class="animated-digits-text">{{dataName}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Statistic',
+    props: {
+        dataNumber: Number,
+        dataUnit: String,
+        dataName: String,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/components/Statistics.vue
+++ frontend/src/components/Statistics.vue
@@ -1,87 +1,36 @@
 <template>
-    <div class="statistics" v-observe-visibility="visibilityChanged">             
-        <!-- Statistics-->
-        <section class="bg-white container-fluid border-top">
-        <div class="row">
-            <div class="col-md-3 col-sm-6 border-right py-5 border-bottom">
-            <div class="animated-digits mx-auto text-center" data-number="350">
-                <h5 class="animated-digits-digit">
-                <span>
-                    <animated-number v-if="isVisible"
-                        :value="350"
-                        :formatValue="formatToPrice"
-                        :duration="1000"
-                    /> 
-                </span>+</h5>
-                <p class="animated-digits-text">Totally satisfied clients</p>
-            </div>
-            </div>
-            <div class="col-md-3 col-sm-6 py-5 border-right border-bottom">
-            <div class="animated-digits mx-auto text-center" data-number="92">
-                <h5 class="animated-digits-digit">
-                    <span>
-                        <animated-number v-if="isVisible"
-                            :value="92"
-                            :formatValue="formatToPrice"
-                            :duration="1000"
-                        />
-                    </span>%</h5>
-                <p class="animated-digits-text">Positive feedback</p>
-            </div>
-            </div>
-            <div class="col-md-3 col-sm-6 py-5 border-right border-bottom">
-            <div class="animated-digits mx-auto text-center" data-number="1446">
-                <h5 class="animated-digits-digit">
-                    <span>
-                    <animated-number v-if="isVisible"
-                        :value="1446"
-                        :formatValue="formatToPrice"
-                        :duration="1000"
-                    />
-                    </span>
-                </h5>
-                <p class="animated-digits-text">Completed projects</p>
-            </div>
-            </div>
-            <div class="col-md-3 col-sm-6 py-5 border-bottom">
-            <div class="animated-digits mx-auto text-center" data-number="28">
-                <h5 class="animated-digits-digit">
-                    $
-                    <span>
-                        <animated-number v-if="isVisible"
-                            :value="28"
-                            :formatValue="formatToPrice"
-                            :duration="1000"
-                        />    
-                    </span>
-                </h5>
-                <p class="animated-digits-text">Average cost per hour</p>
-            </div>
-            </div>
-        </div>
-        </section>
-    </div>
+    <section class="bg-white container-fluid border-top">
+      <div class="row">
+        <Statistic 
+            dataName="Totally satisfied clients"
+            dataNumber=350
+            dataUnit="+"
+        />
+        <Statistic
+            dataName="Positive feedback"
+            dataNumber=92
+            dataUnit="%"
+        />
+        <Statistic
+            dataName="Completed projects"
+            dataNumber=1446
+            dataUnit=''
+        />
+        <Statistic
+            dataName="Average cost per hour"
+            dataNumber=28
+            dataUnit="$"
+        />
+      </div>
+    </section>
 </template>
 
 <script>
-import AnimatedNumber from "animated-number-vue";
+import Statistic from './Statistic'
 export default {
     name: 'Statistics',
-    components:{
-        AnimatedNumber
-    },
-    data: function(){
-        return {
-            isVisible: false
-        }
-    },
-    methods: {
-        visibilityChanged: function(isVisible){
-            this.isVisible = isVisible
-        },
-        formatToPrice : function(value) {
-            return value.toFixed(0)
-        }
+    components: {
+        Statistic
     }
 }
 </script>
